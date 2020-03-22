bb8b18f98c7ae115b69721174b427e4aaeffa9de
MarkBozinovic <42635338+MarkBozinovic@users.noreply.github.com>
2020-03-22 19:59:41 +1100

    Initial commit

---
+++ app/src/main/java/com/vu/nit6120lab4_exercise_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vu.nit6120lab4_exercise_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
